e6b61f528b22174892ab3de30fb78f0dd3a8ef78
mdev9 <devriesmarijn05@gmail.com>
2024-03-23 14:02:22 +0100

    done ex02

--- module_01/ex01/main.cpp
+++ module_01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: marde-vr <marde-vr@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 12:25:30 by marde-vr          #+#    #+#             */
-/*   Updated: 2024/03/23 13:39:30 by marde-vr         ###   ########.fr       */
+/*   Updated: 2024/03/23 13:42:49 by marde-vr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,10 @@
 int	main(void)
 {
 	Zombie* zombies;
-	zombies = zombieHorde(10, "zombie");
+	int		nbOfZombies = 10;
 
-	for (int i = 0; i < 10 ; i++)
+	zombies = zombieHorde(nbOfZombies, "zombie");
+	for (int i = 0; i < nbOfZombies ; i++)
 		zombies[i].announce();
 
 	delete[] zombies;
---
+++ module_01/ex02/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marde-vr <marde-vr@42angouleme.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/23 13:46:22 by marde-vr          #+#    #+#             */
+/*   Updated: 2024/03/23 14:01:04 by marde-vr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(void)
+{
+	std::string	string = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &string;
+	std::string	&stringREF = string;
+
+	std::cout << "Memory address of string: " << &string << std::endl;
+	std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "Memory address held by stringREF: " << &stringREF << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << "String value: " << string << std::endl;
+	std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+}
