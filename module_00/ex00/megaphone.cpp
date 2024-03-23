c91158929fb3f792f0d438b99a06b0ab248f2131
mdev9 <devriesmarijn05@gmail.com>
2024-03-23 11:36:43 +0100

    fixed double new line with no input

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: marde-vr <marde-vr@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 08:05:47 by marde-vr          #+#    #+#             */
-/*   Updated: 2024/03/21 08:33:15 by marde-vr         ###   ########.fr       */
+/*   Updated: 2024/03/23 11:35:36 by marde-vr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 int	main(int ac, char **av)
 {
 	if (ac == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	for (int i = 1; i < ac; i++)
 	{
 		for (size_t j = 0; j < strlen(av[i]); j++)
