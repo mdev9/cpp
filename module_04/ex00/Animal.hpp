aad16bd32257cd7a11f802f3b5e4c79a75e90c54
mdev9 <devriesmarijn05@gmail.com>
2024-05-15 13:56:32 +0200

    fixed destructors

--- module_04/ex00/Animal.hpp
+++ module_04/ex00/Animal.hpp
@@ -6,7 +6,7 @@
 /*   By: marde-vr <marde-vr@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 10:20:53 by marde-vr          #+#    #+#             */
-/*   Updated: 2024/05/15 10:57:39 by marde-vr         ###   ########.fr       */
+/*   Updated: 2024/05/15 13:34:03 by marde-vr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ class	Animal
 
 	public:
 		Animal();
-		~Animal();
+		virtual ~Animal();
 		void	makeSound() const;
 		std::string	getType() const;
 };
--- module_04/ex00/WrongAnimal.hpp
+++ module_04/ex00/WrongAnimal.hpp
@@ -6,7 +6,7 @@
 /*   By: marde-vr <marde-vr@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 10:20:53 by marde-vr          #+#    #+#             */
-/*   Updated: 2024/05/15 11:02:53 by marde-vr         ###   ########.fr       */
+/*   Updated: 2024/05/15 13:34:16 by marde-vr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ class	WrongAnimal
 
 	public:
 		WrongAnimal();
-		~WrongAnimal();
+		virtual	~WrongAnimal();
 		void	makeSound() const;
 		std::string	getType() const;
 };
