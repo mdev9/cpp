4f200ee9945079a196b0b6ce5a744dfee5dec58d
mdev9 <devriesmarijn05@gmail.com>
2024-05-27 10:29:09 +0200

    removed strlen

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -6,12 +6,11 @@
 /*   By: marde-vr <marde-vr@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 08:05:47 by marde-vr          #+#    #+#             */
-/*   Updated: 2024/05/06 15:25:45 by marde-vr         ###   ########.fr       */
+/*   Updated: 2024/05/27 10:28:37 by marde-vr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <cstring>
 
 int	main(int ac, char **av)
 {
@@ -19,7 +18,8 @@ int	main(int ac, char **av)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	for (int i = 1; i < ac; i++)
 	{
-		for (size_t j = 0; j < strlen(av[i]); j++)
+		std::string str = av[i];
+		for (int j = 0; str[j];j++)
 			std::cout << (char)std::toupper(av[i][j]);
 	}
 	std::cout << std::endl;
