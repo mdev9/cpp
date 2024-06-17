71e1e816e9b93eb1f4584a20e0bf1b53a8ca6d87
mdev9 <devriesmarijn05@gmail.com>
2024-06-17 14:13:47 +0200

    very productive, did a lot of things

--- module_06/ex00/ScalarConvert.cpp
+++ module_06/ex00/ScalarConvert.cpp
@@ -6,7 +6,7 @@
 /*   By: marde-vr <marde-vr@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 11:09:04 by marde-vr          #+#    #+#             */
-/*   Updated: 2024/06/17 08:32:53 by marde-vr         ###   ########.fr       */
+/*   Updated: 2024/06/17 14:11:39 by marde-vr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,11 @@ static int stoi( std::string & s ) {
 
 void ScalarConverter::convert(std::string literal)
 {
-	std::cout << "char: " << std::endl;
+	int integer = atoi(literal.c_str());
+	if (integer >= 32 && integer <= 126)
+		std::cout << "char: " << integer << std::endl;
+	else
+		std::cout << "char: Non displayable" << std::endl;
 	//std::cout << "int: " << atoi(literal.c_str()) << std::endl;
 	std::cout << "int: " << stoi(literal) << std::endl;
 	std::cout << "float: " << std::endl;
