5766ed1c9dab2c844d683ef460a78e0946c4cd01
mdev9 <devriesmarijn05@gmail.com>
2024-06-17 08:46:54 +0200

    woip

--- module_06/ex00/ScalarConvert.cpp
+++ module_06/ex00/ScalarConvert.cpp
@@ -6,24 +6,41 @@
 /*   By: marde-vr <marde-vr@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 11:09:04 by marde-vr          #+#    #+#             */
-/*   Updated: 2024/06/15 11:17:24 by marde-vr         ###   ########.fr       */
+/*   Updated: 2024/06/17 08:32:53 by marde-vr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConvert.hpp"
+#include <cstdlib>
+#include <sstream>
 
 ScalarConverter::ScalarConverter() {}
 
-ScalarConverter::ScalarConverter(ScalarConverter &cp) {}
+ScalarConverter::ScalarConverter(ScalarConverter &cp)
+{
+	(void) cp;
+}
 
 ScalarConverter& ScalarConverter::operator=(ScalarConverter &other)
 {
+	(void) other;
 	return *this;
 }
 
 ScalarConverter::~ScalarConverter() {}
 
-void ScalarConverter::convert(std::string litelral)
-{
-
+static int stoi( std::string & s ) {
+    int i;
+    std::istringstream(s) >> i;
+    return i;
 }
+
+void ScalarConverter::convert(std::string literal)
+{
+	std::cout << "char: " << std::endl;
+	//std::cout << "int: " << atoi(literal.c_str()) << std::endl;
+	std::cout << "int: " << stoi(literal) << std::endl;
+	std::cout << "float: " << std::endl;
+	std::cout << "double: " << atof(literal.c_str()) << std::endl;
+}
+
--- module_06/ex00/main.cpp
+++ module_06/ex00/main.cpp
@@ -1,28 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ScalarConvert.hpp                                  :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marde-vr <marde-vr@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/15 11:09:21 by marde-vr          #+#    #+#             */
-/*   Updated: 2024/06/17 08:05:24 by marde-vr         ###   ########.fr       */
+/*   Created: 2024/06/17 08:08:24 by marde-vr          #+#    #+#             */
+/*   Updated: 2024/06/17 08:15:10 by marde-vr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SCALARCONVERT_HPP
-# define SCALARCONVERT_HPP
+#include "ScalarConvert.hpp"
 
-#include <iostream>
-
-class ScalarConverter
+int	main(int argc, char **argv)
 {
-	public:
-		static void convert(std::string literal);
-		ScalarConverter();
-		ScalarConverter(ScalarConverter &cp);
-		ScalarConverter& operator=(ScalarConverter &other);
-		~ScalarConverter();
-};
-
-#endif
+	if (argc > 1)
+	{
+		ScalarConverter sc;
+		sc.convert(argv[1]);
+	}
+}
