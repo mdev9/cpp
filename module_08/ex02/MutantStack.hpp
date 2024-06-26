c40c4fdbe5869adb6c39f2b37de54ca876354ae0
mdev9 <devriesmarijn05@gmail.com>
2024-06-26 10:35:24 +0200

    ex02 done

--- module_08/ex02/MutantStack.hpp
+++ module_08/ex02/MutantStack.hpp
@@ -6,13 +6,35 @@
 /*   By: marde-vr <marde-vr@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 14:12:17 by marde-vr          #+#    #+#             */
-/*   Updated: 2024/06/25 14:13:22 by marde-vr         ###   ########.fr       */
+/*   Updated: 2024/06/26 10:14:00 by marde-vr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
 
+# include <stack>
 
+template <class T>
+class MutantStack: public std::stack<T>
+{
+	public:
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		iterator begin();
+		iterator end();
+
+};
+
+template <class T>
+typename MutantStack<T>::iterator MutantStack<T>::begin()
+{
+	return std::stack<T>::c.begin();
+}
+
+template <class T>
+typename MutantStack<T>::iterator MutantStack<T>::end()
+{
+	return std::stack<T>::c.end();
+}
 
 #endif
---
+++ module_08/ex02/main.cpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marde-vr <marde-vr@42angouleme.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/25 19:01:02 by marde-vr          #+#    #+#             */
+/*   Updated: 2024/06/26 10:20:52 by marde-vr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MutantStack.hpp"
+#include <iostream>
+
+int main()
+{
+	MutantStack<int> mstack;
+
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl; // 3
+	mstack.pop();
+	std::cout << mstack.size() << std::endl; // 1
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
+
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl; // 5 3 5 737 0
+		++it;
+	}
+	std::stack<int> s(mstack);
+	return 0;
+}
